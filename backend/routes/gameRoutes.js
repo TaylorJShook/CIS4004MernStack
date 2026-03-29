e9fe16cd206444c53a40700ea9b7a0b42adbc866
afrechtoasta <lilalisac@gmail.com>
2026-03-29 17:50:44 -0400

    Add the other crud functions to the games api

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getGames } = require("../controllers/gameController");
+const { getGames, getGame, createGame, updateGame, deleteGame } = require("../controllers/gameController");
 
 router.get("/api/games", getGames);
+router.get("/api/games/:gameId", getGame);
+router.post("/api/games", createGame);
+router.put("/api/games/:gameId", updateGame);
+router.delete("/api/games/:gameId", deleteGame);
 
 module.exports = router;
